acb06fc7c1e07da9234f2fe71d75183f17f5076b
Cody Miller <hello@looskie.com>
2023-10-24 17:58:16 -0400

    typo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl -fsSL https://download.hop.sh/install | sh
 
-hop auth login --token YOUR_PATH_TOKEN
+hop auth login --token YOUR_PAT_TOKEN
 hop fwd DEPLOYMENT_ID --project PROJECT_ID --publish 6379 &
 node index.js
